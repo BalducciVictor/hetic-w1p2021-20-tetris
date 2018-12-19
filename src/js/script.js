075a5768d20d6f0b1bb0f7455e1b3abec80f37c3
jshaows <jacky.shaows@outlook.fr>
2018-12-19 09:40:07 +0100

    fix ground

--- src/js/script.js
+++ src/js/script.js
@@ -6,28 +6,28 @@ var gravity = 237;
 var ground;
 
 oxo.inputs.listenKey('space', function() {
-    if (oxo.screens.getCurrentScreen !== 'game') {
-      oxo.screens.loadScreen('game', game);
-    }
-  });
+	if (oxo.screens.getCurrentScreen !== 'game') {
+		oxo.screens.loadScreen('game', game);
+	}
+});
 
-function game () {
-    var cloud = document.getElementById('cloud');
-    var ground = document.getElementById('ground');
-    oxo.elements.onCollisionWithElement(cloud, ground, function() {
-        oxo.screens.loadScreen('end');
-    });
+function game() {
+	var cloud = document.getElementById('cloud');
+	var ground = document.getElementById('ground');
+	oxo.elements.onCollisionWithElement(cloud, ground, function() {
+		oxo.screens.loadScreen('end');
+	});
 
-    for (i = 0; i < gravity; i++) {
-        console.log('ok');
-        setInterval(function() {
-            oxo.animation.move(cloud, 'down', 1.1);
-        }, 800);
-    };
-    oxo.inputs.listenKey('space', function() {
-        oxo.animation.move(cloud, 'up', 100);
-    });
-    
+	for (i = 0; i < gravity; i++) {
+		console.log('ok');
+		setInterval(function() {
+			oxo.animation.move(cloud, 'down', 1.1);
+		}, 800);
+	};
+	oxo.inputs.listenKey('space', function() {
+		oxo.animation.move(cloud, 'up', 100);
+	});
+
 
 
 }
