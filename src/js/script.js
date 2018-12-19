96145c14f17c71c45e3cd830e37f0ba4fc8e181a
jshaows <jacky.shaows@outlook.fr>
2018-12-19 18:45:13 +0100

    fix obstacle

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,15 @@
 var cloud;
 var gravity = 3;
 var ground;
+
 var size = 60;
 var xObstacle = 1280 / 60;
 var yObstacle = 400 / 60;
+var xObstacleFactory = 1280 / 60;
+var yObstacleFactory = 200;
+
 var enemyInterval;
+var factoryInterval;
 
 // Space to start
 oxo.inputs.listenKey('space', function() {
@@ -15,40 +20,14 @@ oxo.inputs.listenKey('space', function() {
 
 function game() {
 
-
-
-	enemyInterval = setInterval(addBlackCloud, 5000);
-
-	// // Spawn factory
-	// var factory = oxo.elements.createElement({
-	// 	type: 'div',
-	// 	class: 'factory',
-	// 	styles: {
-	// 		height: '233px',
-	// 		width: '419px',
-	// 		transform: 'translate(1280px, 523px)',
-	// 	}
-	// });
-
-	// var moveInterval = setInterval(function() {
-	// 	oxo.animation.move(factory, 'left', 2, true);
-	// }, 40);
-
-	
-	// oxo.elements.onLeaveScreenOnce(factory, function() {
-	// 	factory.remove();
-	// 	clearInterval(moveInterval);
-	// }, true);
-
-
-
+	enemyInterval = setInterval(addBlackCloud, 10000);
+	factoryInterval = setInterval(addFactory, 15000);
 
 	// Start scoring
 	oxo.player.setScore(0);
 	timer = setInterval(function() {
 		oxo.player.addToScore(1);
 	}, 500);
-
 
 	var cloud = document.getElementById('cloud');
 	oxo.animation.setPosition(cloud, { x: 0, y: 350 });
@@ -68,10 +47,73 @@ function game() {
 		oxo.animation.move(cloud, 'down', gravity);
 	}, 10);
 
-	// Collision with foor
-	// oxo.elements.onCollisionWithElement(cloud, factory, function() {
-	// 	oxo.screens.loadScreen('end');
-	// 	clearInterval(timer);
+	oxo.elements.onCollisionWithElement(cloud, ground, end);
+}
+
+function addBlackCloud(){
+	var obstacle = oxo.elements.createElement({
+		class: 'blackCloud',
+		styles: {
+			transform:
+          "translate(" +
+          (oxo.utils.getRandomNumber(0, xObstacle - 1) * size + 1280) +
+          "px, " +
+          oxo.utils.getRandomNumber(0, yObstacle - 1) * size + 
+          "px)"
+      },
+      appendTo: '#sky'
+	});
+		
+	var moveInterval = setInterval(function() {
+		oxo.animation.move(obstacle, 'left', 2, true);
+	}, 10);
+
+	oxo.elements.onCollisionWithElement(cloud, obstacle, end);
+	
+}
+
+function addFactory(){
+	var obstacle = oxo.elements.createElement({
+		class: 'factory',
+		styles: {
+			transform:
+          "translate(" +
+          (oxo.utils.getRandomNumber(0, xObstacleFactory - 1) * size + 1280) +
+          "px, " +
+          (83) + 
+          "px)"
+      },
+      appendTo: '#factory__zone'
+	});
+		
+	var moveInterval = setInterval(function() {
+		oxo.animation.move(obstacle, 'left', 2, true);
+	}, 10);
+
+		oxo.elements.onCollisionWithElement(cloud, obstacle, end);
+}
+
+
+
+
+
+function end() {
+// Collision with foor
+	oxo.screens.loadScreen('end');
+	clearInterval(timer);
+	clearInterval(factoryInterval);
+	clearInterval(enemyInterval);
+
+	// Reset space 
+	oxo.inputs.listenKey('space', function() {
+		if (oxo.screens.getCurrentScreen !== 'game') {
+			oxo.screens.loadScreen('game', game);
+		}
+	});
+}
+
+
+
 
 	// 	// Reset space 
 	// 	oxo.inputs.listenKey('space', function() {
@@ -81,51 +123,4 @@ function game() {
 	// 	});
 	// });
 
-	// // 
-
-	oxo.elements.onCollisionWithElement(cloud, ground, function() {
-		// oxo.screens.loadScreen('end');
-		clearInterval(timer);
-
-		// Reset space 
-		oxo.inputs.listenKey('space', function() {
-			if (oxo.screens.getCurrentScreen !== 'game') {
-				oxo.screens.loadScreen('game', game);
-			}
-		});
-	});
-
-
-}
-
-
-function addBlackCloud(){
-	var obstacle = oxo.elements.createElement({
-		type: 'div',
-		class: 'blackCloud',
-		styles: {
-			transform:
-          "translate(" +
-          (oxo.utils.getRandomNumber(0, xObstacle - 1) * size + 1280) +
-          "px, " +
-          oxo.utils.getRandomNumber(0, yObstacle - 1) * size +
-          "px)"
-      },
-	});
-	var moveInterval = setInterval(function() {
-		oxo.animation.move(obstacle, 'left', 2, true);
-	}, 10);
-
-
-	oxo.elements.onCollisionWithElement(cloud, obstacle, function() {
-		oxo.screens.loadScreen('end');
-		clearInterval(timer);
-
-		// Reset space 
-		oxo.inputs.listenKey('space', function() {
-			if (oxo.screens.getCurrentScreen !== 'game') {
-				oxo.screens.loadScreen('game', game);
-			}
-		});
-	});
-}
+	// 
