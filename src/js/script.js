4804adc85fa2fa896b4b0d009dab87a1f2ec2e1f
jshaows <jacky.shaows@outlook.fr>
2018-12-20 17:32:51 +0100

    add soundEffect

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,9 @@
 var cloud;
 var gravity = 3;
 var ground;
-
 var size = 60;
 var xObstacle = 1280 / 60;
 var yObstacle = 400 / 60;
-var xObstacleFactory = 1280 / 60;
-var yObstacleFactory = 200;
-
-
 var enemyInterval;
 var factoryInterval;
 var moveInterval;
@@ -26,18 +21,22 @@ oxo.inputs.listenKey('space', function() {
 
 function game() {
 
+	// Set volume sound of game 
+	var gameSound = document.getElementById('gameSound');
+	gameSound.volume = 0.5;
+
 	enemyInterval = setInterval(addBlackCloud, 5000);
-	factoryInterval = setInterval(addFactory, 5000);
+	factoryInterval = setInterval(addFactory, 6000);
 	planeInterval = setInterval(addPlane, 5000);
 
-	oxo.inputs.listenKeys(["q", "z", "d", "a"], function(key){
+	oxo.inputs.listenKeys(["q", "z", "d", "a"], function(key) {
 		var position = oxo.animation.getPosition(cloud);
 		if (!canShoot) {
 			return;
 		}
 		canShoot = false;
-		setTimeout(function(){
-				canShoot = true;
+		setTimeout(function() {
+			canShoot = true;
 
 		}, 1500);
 
@@ -45,44 +44,58 @@ function game() {
 			var shoot = oxo.elements.createElement({
 				class: 'thunder',
 				styles: {
-					transform: "translate(" +	(250) +"px, " + position.y  + "px)"
+					transform: "translate(" + (250) + "px, " + position.y + "px)"
 				},
 				appendTo: '#sky'
 			});
 
 			boltInterval = setInterval(function() {
 				oxo.animation.move(shoot, 'right', 2, true);
-			}, 10);
+			}, 3);
 
-
-			oxo.elements.onCollisionWithElement(shoot, obstacle, function(){
-				oxo.player.addToScore(10);
+			oxo.elements.onLeaveScreen(shoot, function() {
 				shoot.remove();
-				obstacle.remove();
+			}, true);
+
+			var obstacles = document.querySelectorAll('.move');
+
+			obstacles.forEach(function(obstacle) {
+				oxo.elements.onCollisionWithElement(shoot, obstacle, function() {
+					oxo.player.addToScore(10);
+					shoot.remove();
+					obstacle.remove();
+					boltSound();
+				});
 			});
-			
 
 		}
 		if (key === "a") {
-			var shoot = oxo.elements.createElement({
+			var drop = oxo.elements.createElement({
 				class: 'rain',
 				styles: {
-					transform: "translate(" +	(250) +"px, " + position.y  + "px)"
+					transform: "translate(" + (250) + "px, " + position.y + "px)"
 				},
 				appendTo: '#sky'
 			});
 
 			boltInterval = setInterval(function() {
-				oxo.animation.move(shoot, 'down', 2, true);
-			}, 10);
+				oxo.animation.move(drop, 'down', 2, true);
+			}, 3);
 
-
-			oxo.elements.onCollisionWithElement(shoot, obstacle, function(){
-				oxo.player.addToScore(10);
-				shoot.remove();
-				obstacle.remove();
+			oxo.elements.onCollisionWithElement(drop, ground, function() {
+				drop.remove();
 			});
-			
+
+			var obstacles = document.querySelectorAll('.move');
+
+			obstacles.forEach(function(obstacle) {
+				oxo.elements.onCollisionWithElement(drop, obstacle, function() {
+					oxo.player.addToScore(10);
+					drop.remove();
+					obstacle.remove();
+					waterSound();
+				});
+			});
 
 		}
 
@@ -92,7 +105,7 @@ function game() {
 	oxo.player.setScore(0);
 	timer = setInterval(function() {
 		oxo.player.addToScore(1);
-	}, 500);
+	}, 10000);
 
 	cloud = document.getElementById('cloud');
 	oxo.animation.setPosition(cloud, { x: 0, y: 350 });
@@ -113,7 +126,7 @@ function game() {
 	}, 10);
 
 	oxo.elements.onCollisionWithElement(cloud, ground, end);
-	
+
 }
 
 function addBlackCloud() {
@@ -131,19 +144,25 @@ function addBlackCloud() {
 
 	facInterval = setInterval(function() {
 		oxo.animation.move(obstacle, 'left', 2, true);
-	}, 10);
+	}, 3);
 
-	oxo.elements.onLeaveScreenOnce(obstacle, remove);
+	oxo.elements.onLeaveScreen(obstacle, function() {
+		if (oxo.animation.getPosition(obstacle).x < 0) {
+			obstacle.remove();
+
+		}
+	}, true);
 
 	oxo.elements.onCollisionWithElement(cloud, obstacle, end);
- 
+
 }
+
 function addFactory() {
-	 var obstacle = oxo.elements.createElement({
+	var obstacle = oxo.elements.createElement({
 		class: 'factory move',
 		styles: {
 			transform: "translate(" +
-				(oxo.utils.getRandomNumber(0, xObstacleFactory - 1) * size + 1280) +
+				(oxo.utils.getRandomNumber(0, xObstacle - 1) * size + 1280) +
 				"px, " +
 				(83) +
 				"px)"
@@ -153,49 +172,54 @@ function addFactory() {
 
 	facInterval = setInterval(function() {
 		oxo.animation.move(obstacle, 'left', 2, true);
-	}, 10);
+	}, 8);
 
-	oxo.elements.onLeaveScreenOnce(obstacle, remove);
+	oxo.elements.onLeaveScreen(obstacle, function() {
+		if (oxo.animation.getPosition(obstacle).x < 0) {
+			obstacle.remove();
+
+		}
+	}, true);
 
 	oxo.elements.onCollisionWithElement(cloud, obstacle, end);
 }
 
 
 function addPlane() {
-  if (oxo.player.getScore() > 300) {
-    var obstacle = oxo.elements.createElement({
-      class: "plane move",
-      obstacle: true,
-      styles: {
-        transform: "translate(" +
-				(oxo.utils.getRandomNumber(0, xObstacle - 1) * size + 1280) +
-				"px, " +
-				oxo.utils.getRandomNumber(0, yObstacle - 1) * size +
-				"px)"
-		},
-		appendTo: '#sky'
-    });
+	if (oxo.player.getScore() > 150) {
+		var obstacle = oxo.elements.createElement({
+			class: "plane move",
+			styles: {
+				transform: "translate(" +
+					(oxo.utils.getRandomNumber(0, xObstacle - 1) * size + 1280) +
+					"px, " +
+					oxo.utils.getRandomNumber(0, yObstacle - 1) * size +
+					"px)"
+			},
+			appendTo: '#sky'
+		});
 
-    planeInterval = setInterval(function() {
-		oxo.animation.move(obstacle, 'left', 2, true);
-	}, 10);
+		planeInterval = setInterval(function() {
+			oxo.animation.move(obstacle, 'left', 2, true);
+		}, 10);
 
-    oxo.elements.onLeaveScreenOnce(obstacle, remove);
+		oxo.elements.onLeaveScreen(obstacle, function() {
+			if (oxo.animation.getPosition(obstacle).x < 0) {
+				obstacle.remove();
 
-    oxo.elements.onCollisionWithElement(cloud, obstacle, end);
-  }
+			}
+		}, true);
+
+		oxo.elements.onCollisionWithElement(cloud, obstacle, end);
+	}
 }
 
+function boltSound() {
+	document.getElementById('bolt').play();
+}
 
-function remove() {
-	var allMovableElements = document.querySelectorAll(".move");
-	for (let i = 0; i < allMovableElements.length; i++) {
-		//get position.x to  if it is out of the screen (on the left)
-		var position = oxo.animation.getPosition(allMovableElements[i]);
-		if (position.x < -200) {
-			allMovableElements[i].remove();
-		}
-	}
+function waterSound() {
+	document.getElementById('water').play();
 }
 
 function end() {
